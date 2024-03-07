d5e83e25a2e028def816eefcb02b257336ee59ec
tamarceleste <tamarceleste@gmail.com>
2024-03-06 21:11:59 -0300

    primer gráfico

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el precio promedio de cada marca de auto')
             
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+  
+    fig = px.histogram(car_data, x="marca", y="price", histfunc='avg')
+    fig.show()
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
